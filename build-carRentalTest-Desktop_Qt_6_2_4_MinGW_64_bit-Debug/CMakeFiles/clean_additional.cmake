8d0202d7f677c5b2d970ea85251fd5efaae9b81a
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-04-29 11:32:38 +0200

    ingenting

---
+++ build-carRentalTest-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\carRentalTest_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\carRentalTest_autogen.dir\\ParseCache.txt"
+  "carRentalTest_autogen"
+  )
+endif()
